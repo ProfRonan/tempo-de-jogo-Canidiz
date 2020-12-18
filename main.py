245f4d1b154a740ed1a9badd8f41e081beb370e0
Canidiz <pmiguelneto@hotmail.com>
2020-12-18 04:14:49 +0000

    não passa teste, duvida no canal geral discord

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
 """Arquivo principal que será interpretado pelo interpretador."""
+from datetime import datetime, time as datetime_time, timedelta
 
 
 def main():
-    """Função principal que será rodada quando o script for passado para o interpretador."""
-    # COLOQUE SEU CÓDIGO AQUI
+    tempo1 = input("Digite o horário de início\n")
+    tempo2 = input("Digite o horário de conclusão\n")
+
+    for time_range in [tempo1,tempo2]: 
+
+        s = datetime.strptime(tempo1, '%H:%M:%S')
+        e = datetime.strptime(tempo2, '%H:%M:%S')
+
+        print(diferenca_tempo(s, e))
+        assert diferenca_tempo(s, e) == diferenca_tempo(s.time(), e.time())
+ 
+
+def diferenca_tempo(comeco, fim):
+    if isinstance(comeco, datetime_time): 
+        assert isinstance(fim, datetime_time)
+        comeco, fim = [datetime.combine(datetime.min, t) for t in [comeco, fim]]
+    if comeco <= fim: 
+        return fim - comeco
+    else: 
+        fim += timedelta(1) 
+        assert fim > comeco
+        return fim - comeco
+
 
 
 if __name__ == '__main__':
