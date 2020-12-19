1204a8e77f847b1684ab8d34c201c380f1c8e40f
Canidiz <pmiguelneto@hotmail.com>
2020-12-19 22:10:28 +0000

    Entendi o problema do for, realmente eu não tinha pensado muito bem ao usar o mesmo.
    em relação a mudar o import acontece um erro um pouco estranho, eu não entendo 100% mas sei que é relacionado a linha 26, se eu tentar trocar "datetime_time" apenas por "time" não funciona. (acredito que eu tenha que atribuir o mesmo a algo)
    
    em relação a utilizar o "strftime" o mesmo aparece parece não "existir" e usando o "strptime" já utilizado antes não funciona.
    
    acho que eu sei onde está meu erro, acredito que seja na linha 27 e na linha 31, provavelmente elas alteram algo que eu estou deixando passar na variável, irei procurar uma maneira diferente de fazer o mesmo!

--- main.py
+++ main.py
@@ -2,29 +2,34 @@
 from datetime import datetime, time as datetime_time, timedelta
 
 
+
 def main():
+    global j
+
     tempo1 = input("Digite o horário de início\n")
     tempo2 = input("Digite o horário de conclusão\n")
 
-    for time_range in [tempo1,tempo2]: 
 
-        s = datetime.strptime(tempo1, '%H:%M:%S')
-        e = datetime.strptime(tempo2, '%H:%M:%S')
+    s = datetime.strptime(tempo1,  '%H:%M:%S')
+    e = datetime.strptime(tempo2, '%H:%M:%S')
+    diferenca_tempo(s,e)
 
-        print(diferenca_tempo(s, e))
-        assert diferenca_tempo(s, e) == diferenca_tempo(s.time(), e.time())
+    
+    print(j)
+    
+        
  
 
 def diferenca_tempo(comeco, fim):
+    global j
+
     if isinstance(comeco, datetime_time): 
-        assert isinstance(fim, datetime_time)
         comeco, fim = [datetime.combine(datetime.min, t) for t in [comeco, fim]]
     if comeco <= fim: 
-        return fim - comeco
+        j = fim - comeco
     else: 
         fim += timedelta(1) 
-        assert fim > comeco
-        return fim - comeco
+        j = fim - comeco
 
 
 
