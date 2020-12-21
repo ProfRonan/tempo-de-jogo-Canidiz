cae3eeb7ebd7b2b367662cf3e5b003bd7356b3f5
SeuRonão <22750712+SeuRonao@users.noreply.github.com>
2020-12-21 20:22:38 -0300

    Alterações para tentar fazer funcionar usando datetime
    Ainda faltam coisas para consertar, mas acho que você consegue o resto.
    Qualquer coisa, avise no github/discord.

--- main.py
+++ main.py
@@ -1,36 +1,23 @@
 """Arquivo principal que será interpretado pelo interpretador."""
-from datetime import datetime, time as datetime_time, timedelta
-
-
+import datetime
 
 def main():
-    global j
 
     tempo1 = input("Digite o horário de início\n")
     tempo2 = input("Digite o horário de conclusão\n")
 
-
-    s = datetime.strptime(tempo1,  '%H:%M:%S')
-    e = datetime.strptime(tempo2, '%H:%M:%S')
-    diferenca_tempo(s,e)
-
-    
-    print(j)
-    
-        
- 
-
-def diferenca_tempo(comeco, fim):
-    global j
-
-    if isinstance(comeco, datetime_time): 
-        comeco, fim = [datetime.combine(datetime.min, t) for t in [comeco, fim]]
-    if comeco <= fim: 
-        j = fim - comeco
-    else: 
-        fim += timedelta(1) 
-        j = fim - comeco
-
+    sd = datetime.datetime(1,1,1) # Cria uma data de referência.
+    ed = datetime.datetime(1,1,1) # Cria uma data de referência.
+    s = datetime.time(*map(int, tempo1.split(':'))) # Cria um tempo baseado na entrada do usuário
+    e = datetime.time(*map(int, tempo2.split(':'))) # Cria um tempo baseado na entrada do usuário
+    start = datetime.datetime.combine(sd, s) # Junta a data com o tempo.
+    end = datetime.datetime.combine(ed, e) # Junta a data com o tempo.
+    # É necessário o combine por conta de não poder fazer somas e subtrações utilizando o tempo sem a data embutida.
+    if end < start:
+        # Caso o usuário tenha colocado um horário final menor que o inicial, então é pra somar um dia na data do fim.
+        end = end + datetime.timedelta(days=1)
+    interval = end - start
+    print(str(interval))
 
 
 if __name__ == '__main__':
